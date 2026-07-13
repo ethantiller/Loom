50ee8153a55ae16d8222d7d0637937c548f7a835
Ethan Tiller <tiller.ethan05@gmail.com>
2026-07-13 18:51:34 -0400

    add welcome back, ethan

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,14 +8,9 @@ export default function Home() {
       <div className="flex flex-1 items-center justify-center bg-zinc-800 px-6">
         <main className="flex w-full max-w-sm flex-col items-center gap-7 text-center">
           <div>
-            <Image
-              className="invert"
-              src="/loom-logo.png"
-              alt="Loom logo"
-              width={400}
-              height={400}
-              priority
-            />
+            <p className="text-3xl font-semibold text-zinc-300">
+              Welcome back, <span className="font-medium text-zinc-300">Ethan</span>
+            </p>
           </div>
         </main>
       </div>
